8083dbf281587c7d9c4a8ca72ae9f5db77d7f2ff
Ethan Sagin <ethansagin@gmail.com>
2019-04-18 15:10:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,20 @@
 class Song
-attr_reader :name, :artist, :genre
+  attr_reader :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@genre << genre
+    @@count += 1
+    
+  end
+  
+  @@count = 0
+  @@genres = []
+  
+  def count
+    @@count
   end
   
 end
