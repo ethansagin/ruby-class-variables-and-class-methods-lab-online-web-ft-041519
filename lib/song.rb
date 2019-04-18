660807c9bd069976d029ad4e7761adcd41e1650e
Ethan Sagin <ethansagin@gmail.com>
2019-04-18 15:13:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,18 @@
 class Song
   attr_reader :name, :artist, :genre
 
+  @@count = 0
+  @@genres = []
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@genre << genre
     @@count += 1
-    
+    @@genre.include?(genre) ? @@genre:@@genre << genre
   end
   
-  @@count = 0
-  @@genres = []
+
   
   def count
     @@count
